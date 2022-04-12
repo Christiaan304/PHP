784e4ab29d7102db8deb8055886bc109b83eb81d
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-04-12 04:44:59 -0300

    .

--- crud/php-action/create.php
+++ crud/php-action/create.php
@@ -5,12 +5,23 @@
     //conexão 
     require_once 'db-connect.php';
 
+    //função para evitar ataque xss
+    function clear($input)
+    {
+        global $connect;
+        // sql
+        $var = mysqli_escape_string($connect, $input);
+        // xss  
+        $var = htmlspecialchars($var);
+        return $var;
+    }
+
     if(isset($_POST['btn-register']))
     {
-        $name = mysqli_escape_string($connect, ucwords(strtolower($_POST['name'])));   
-        $surname = mysqli_escape_string($connect, ucwords(strtolower($_POST['surname'])));
-        $email = mysqli_escape_string($connect, strtolower($_POST['email']));
-        $age = mysqli_escape_string($connect, $_POST['age']);
+        $name = clear(ucwords(strtolower($_POST['name'])));   
+        $surname = clear(ucwords(strtolower($_POST['surname'])));
+        $email = clear(strtolower($_POST['email']));
+        $age = clear($_POST['age']);
 
         $sql = "INSERT INTO client (name, surname, email, age) VALUES ('$name', '$surname', '$email', '$age')";
 
--- crud/php-action/update.php
+++ crud/php-action/update.php
@@ -5,13 +5,24 @@
     //conexão 
     require_once 'db-connect.php';
 
+    //função para evitar ataque xss
+    function clear($input)
+    {
+        global $connect;
+        // sql
+        $var = mysqli_escape_string($connect, $input);
+        // xss  
+        $var = htmlspecialchars($var);
+        return $var;
+    }
+
     if(isset($_POST['btn-edit']))
     {
-        $name = mysqli_escape_string($connect, ucwords(strtolower($_POST['name'])));   
-        $surname = mysqli_escape_string($connect, ucwords(strtolower($_POST['surname'])));
-        $email = mysqli_escape_string($connect, strtolower($_POST['email']));
-        $age = mysqli_escape_string($connect, $_POST['age']);
-        $id = mysqli_escape_string($connect, $_POST['id']);
+        $name = clear(ucwords(strtolower($_POST['name'])));   
+        $surname = clear(ucwords(strtolower($_POST['surname'])));
+        $email = clear(strtolower($_POST['email']));
+        $age = clear($_POST['age']);
+        $id = clear($_POST['id']);
 
         $sql = "UPDATE client SET name = '$name', surname = '$surname', email = '$email', age = '$age' WHERE id = '$id'";
 
